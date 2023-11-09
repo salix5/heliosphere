aae70dae2cb4ce768ffaa87599cce79fdf428060
salix5 <salix5566@gmail.com>
2023-11-09 16:33:36 +0800

    remove GUILD_ID

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,10 +2,8 @@ import { REST, Routes } from 'discord.js';
 import { readdirSync } from 'node:fs';
 
 const clientId = process.env.CLIENT_ID;
-const guildId = process.env.GUILD_ID;
 const token = process.env.TOKEN;
 const public_commands = [];
-const test_commands = [];
 
 const commandsURL = new URL('commands/', import.meta.url);
 const commandFiles = readdirSync(commandsURL).filter(file => file.endsWith('.js'));
@@ -18,9 +16,6 @@ for (const file of commandFiles) {
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 const commands = await Promise.all(import_list);
 for (const command of commands) {
-	if (command.is_test)
-		test_commands.push(command.data.toJSON());
-	else
 		public_commands.push(command.data.toJSON());
 }
 
@@ -42,18 +37,6 @@ const rest = new REST({ version: '10' }).setToken(token);
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
 		// And of course, make sure you catch and log any errors!
-		console.error(error);
-	}
-	try {
-		console.log(`Started refreshing ${test_commands.length} test commands.`);
-
-		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
-			{ body: test_commands },
-		);
-
-		console.log(`Successfully reloaded ${data.length} test commands.`);
-	} catch (error) {
 		console.error(error);
 	}
 })();
