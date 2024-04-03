7e4f380d427a42f065582a08c145ef1c24094f8e
salix5 <salix5566@gmail.com>
2024-04-03 15:39:56 +0800

    bot: add frequency map

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 import { Client, Collection, Events, GatewayIntentBits, ChannelType, MessageType, Partials } from 'discord.js';
 import { readdirSync } from 'node:fs';
 
-const re_wildcard = /(^|[^\$])[%_]/;
+// eslint-disable-next-line no-unused-vars
+const re_wildcard = /(^|[^$])[%_]/;
 
 const client = new Client({
 	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages],
@@ -10,6 +11,7 @@ const client = new Client({
 
 client.cooldowns = new Collection();
 client.commands = new Collection();
+client.frequency = new Collection();
 const commandsURL = new URL('commands/', import.meta.url);
 const commandFiles = readdirSync(commandsURL).filter(file => file.endsWith('.js'));
 const import_list = [];
@@ -21,11 +23,12 @@ for (const file of commandFiles) {
 }
 
 const commands = await Promise.all(import_list);
-for (let i = commands.length - 1; i >= 0; --i){
+for (let i = commands.length - 1; i >= 0; --i) {
 	const command = commands[i];
 	const commandURL = url_list[i];
 	if ('data' in command && 'execute' in command) {
 		client.commands.set(command.data.name, command);
+		client.frequency.set(command.data.name, 0);
 	} else {
 		console.log(`[WARNING] The command at ${commandURL} is missing a required "data" or "execute" property.`);
 	}
@@ -64,11 +67,16 @@ client.on(Events.InteractionCreate, async interaction => {
 			return;
 		}
 
-		const { cooldowns } = interaction.client;
+		const { cooldowns, frequency } = interaction.client;
 		if (!cooldowns.has(command.data.name)) {
 			cooldowns.set(command.data.name, new Collection());
-			console.log('start:',command.data.name);
+			console.log('start:', command.data.name);
 		}
+		let x = frequency.get(command.data.name);
+		++x;
+		frequency.set(command.data.name, x);
+		if (x % 10 === 0)
+			console.log(`#${command.data.name}:`, x);
 
 		const defaultCooldownDuration = 0;
 		const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
@@ -103,10 +111,11 @@ client.on(Events.InteractionCreate, async interaction => {
 			try {
 				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 			}
-			catch { }
+			catch { /* empty */ }
 		}
 	}
 	else if (interaction.isAutocomplete()) {
+		const begin = Date.now();
 		const command = interaction.client.commands.get(interaction.commandName);
 		if (!command) {
 			console.error(`No command matching ${interaction.commandName} was found.`);
@@ -117,7 +126,11 @@ client.on(Events.InteractionCreate, async interaction => {
 			await command.autocomplete(interaction);
 		}
 		catch (error) {
-			console.error(interaction.commandName, interaction.options.getFocused());
+			const fail_time = new Date();
+			const begin_time = new Date(begin);
+			console.error(`autocomplete in ${interaction.commandName}:`, interaction.options.getFocused());
+			console.error('begin at', begin_time.toTimeString());
+			console.error('fail at', fail_time.toTimeString());
 			console.error(error);
 		}
 	}
