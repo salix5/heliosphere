89ef09def90e08c8af4746f5ecd58d518e85b5bd
salix5 <salix5566@gmail.com>
2024-07-03 15:16:26 +0800

    fix import json

--- common-json-loader.mjs
+++ common-json-loader.mjs
@@ -1,4 +1,4 @@
-import ruby_json from './commands_data/choices_ruby.json' assert { type: 'json' };
+import ruby_json from './commands_data/choices_ruby.json' with { type: 'json' };
 const ruby_entries = Object.entries(ruby_json);
 
 export {
